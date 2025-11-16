76b1d17a7bcb363911185c82006a96d29a8494e8
Lukáš Čaniga <lukas.caniga@gmail.com>
2025-11-16 21:52:18 +0100

    fix: improve Jest setup to handle globalThis.localStorage deletion more robustly

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,9 @@
-// Jest setup file to ensure no global localStorage exists in Node.js environment
-if (typeof global.localStorage !== 'undefined') {
-  try { delete global.localStorage; } catch (e) { global.localStorage = undefined; }
-}
-if (typeof globalThis.localStorage !== 'undefined') {
-  try { delete globalThis.localStorage; } catch (e) { globalThis.localStorage = undefined; }
+// Jest setup file to ensure no globalThis.localStorage exists in Node.js environment
+if (globalThis.localStorage !== undefined) {
+  // Only attempt to delete if configurable, otherwise set to undefined
+  try {
+    delete globalThis.localStorage;
+  } catch {
+    globalThis.localStorage = undefined;
+  }
 }
